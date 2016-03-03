f9adec6d0ef26ed0c79953a7d378d4fc83770843
Slava Vishnyakov <bomboze@gmail.com>
2016-03-03 18:18:22 +0300

    Fix fields in response structure

--- src/Provider.php
+++ src/Provider.php
@@ -18,10 +18,21 @@ class Provider extends AbstractProvider
 
         $user = $this->server->getUserDetails($token = $this->getToken());
 
-        return (new User())->setRaw($user->extra)->map([
-            'id' => $user['key'], 'nickname' => $user['name'],
-            'name' => $user['name'], 'email' => $user['email'],
-            'avatar' => array_get($user, 'avatar'),
+        $userObject = (new User());
+
+        if (isset($user['extra'])) {
+            $userObject = $userObject->setRaw($user['extra']);
+        }
+
+        return $userObject->map([
+            'id' => array_get($user, 'key'),
+            'nickname' => array_get($user, 'nickname', array_get($user, 'name')),
+            'name' => array_get($user, 'displayName', array_get($user, 'name')),
+            'email' => array_get($user, 'emailAddress', array_get($user, 'email')),
+            'avatar' => array_get($user, 'avatarUrls.48x48', array_get($user, 'avatar')),
+            'active' => array_get($user, 'active'),
+            'timezone' => array_get($user, 'timeZone'),
+            'locale' => array_get($user, 'locale'),
         ])->setToken($token->getIdentifier(), $token->getSecret());
     }
 }
